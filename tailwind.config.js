a1055a7f76ee9a27e5ee2cbd699c9f7037aa7a5e
Rahul Chauhan <rahulchauhan962103@gmail.com>
2025-05-23 15:00:26 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+     extend: {
+      fontFamily: {
+        sans: ["Open Sans"]
+      },
+      gridTemplateColumns: {
+        "1/5": "1fr 5fr"
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
+
